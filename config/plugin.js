d8cbac0eaa2b4bdb2b26bdb77ec038edbc82ea15
tinyt98 <17754970364@163.com>
2020-06-16 23:20:01 +0800

    模板文件上传

---
+++ config/plugin.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+// /** @type Egg.EggPlugin */
+// module.exports = {
+//   // had enabled by egg
+//   // static: {
+//   //   enable: true,
+//   // }
+// };
+exports.ejs = {
+  enable: true, package: 'egg-view-ejs',
+};
