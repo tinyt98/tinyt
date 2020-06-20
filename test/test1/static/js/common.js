9301a17129499d0a58be1dbcb374bd9ccfedac2e
tinyt98 <2069644049@qq.com>
2020-06-20 17:51:33 +0800

    推荐级更新

--- test/test1/static/js/common.js
+++ test/test1/static/js/common.js
@@ -14,18 +14,5 @@ $(function () {
     var mySwiper2 = new Swiper('.swiper-container2', {
         slidesPerView: 1,
         spaceBetween: 40,
-        breakpoints: {
-            320: {  //当屏幕宽度大于等于320
-                slidesPerView: 2,
-                spaceBetween: 10
-            },
-            768: {  //当屏幕宽度大于等于768 
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            1280: {  //当屏幕宽度大于等于1280
-                slidesPerView: 4,
-                spaceBetween: 30
-            }
-        }
-})})
+    })
+})
