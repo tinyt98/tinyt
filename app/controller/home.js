07f01cf22f83ea87f336b2661d538d2ca5292e40
tinyt98 <2069644049@qq.com>
2020-06-28 23:07:26 +0800

    安装平配置数据库可用

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,7 +6,10 @@ class HomeController extends Controller {
   async index() {
     await this.ctx.render('index',{
       csrf:this.ctx.csrf
-    })
+    });
+    var data = await  this.app.mysql.get('noob', {id:1}); 
+    console.log(data);
+    
 
   }
   async login() {
--- config/plugin.js
+++ config/plugin.js
@@ -1,12 +1,14 @@
-// 'use strict';
+'use strict';
 
-// /** @type Egg.EggPlugin */
-// module.exports = {
-//   // had enabled by egg
-//   // static: {
-//   //   enable: true,
-//   // }
-// };
-exports.ejs = {
-  enable: true, package: 'egg-view-ejs',
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  ejs: {
+    enable: true,
+    package: 'egg-view-ejs'
+  },
+  mysql:{
+    enable: true,
+    package: 'egg-mysql'
+  }
 };
